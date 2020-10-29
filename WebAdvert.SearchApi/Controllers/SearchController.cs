9215b667815df822eb93cd881712eb23096f45c9
Shyam Menon <shyam.s.menon@gmail.com>
2020-10-29 06:24:09 +0530

    Searh worker Api changes

---
+++ WebAdvert.SearchApi/Controllers/SearchController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebAdvert.SearchApi.Models;
+using WebAdvert.SearchApi.Services;
+
+namespace WebAdvert.SearchApi.Controllers
+{
+    [Route("search/v1")]
+    [ApiController]
+    [Produces("application/json")]
+    public class SearchController : Controller
+    {
+        private readonly ISearchService _searchService;
+        private readonly ILogger<SearchController> _logger;
+
+        public SearchController(ISearchService searchService, ILogger<SearchController> logger)
+        {
+            _searchService = searchService;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("{keyword}")]
+        public async Task<List<AdvertSearchType>> Get(string keyword)
+        {
+            _logger.LogInformation($"Search was called with keyword {keyword}");
+            return await _searchService.Search(keyword);
+        }
+    }
+}
---
+++ WebAdvert.SearchApi/Services/SearchService.cs
@@ -0,0 +1,25 @@
+﻿using Nest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAdvert.SearchApi.Models;
+
+namespace WebAdvert.SearchApi.Services
+{
+    public class SearchService : ISearchService
+    {
+        private readonly IElasticClient _client;
+        public SearchService(IElasticClient client)
+        {
+            _client = client;
+        }
+        public async Task<List<AdvertSearchType>> Search(string keyword)
+        {
+            var searchResponse = await _client.SearchAsync<AdvertSearchType>
+                 (search => search.Query(query => query.Term(field => field.Title, keyword.ToLower())
+                 ));
+            return searchResponse.Hits.Select(hit => hit.Source).ToList();
+        }
+    }
+}
